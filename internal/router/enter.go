b24e1e41d7499c7f0800ec4fee296bf2faf31435
unclesam <2036253946@qq.com>
2026-07-08 03:38:03 +0800

    style(router): 移除注释中的emoji

--- internal/router/enter.go
+++ internal/router/enter.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// RegisterRoutes 💡 主路由入口，注册中间件并分发子路由
+// RegisterRoutes 主路由入口，注册中间件并分发子路由
 func RegisterRoutes(r *gin.Engine) {
 	// 分组并调用子路由
 	g := r.Group("api/v1")
